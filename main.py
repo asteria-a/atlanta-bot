683ba4d860dadccd4c7ad9661e56de9af9288738
asteria-a <trickyheadz.pengo@gmail.com>
2024-02-20 13:18:04 +0100

    cba to make updates xd

--- main.py
+++ main.py
@@ -1,83 +1,164 @@
-import requests
 import discord
-from discord import app_commands
-from discord.ext import commands
-import json
-import time
+from discord import app_commands, File
+from discord.ext import commands, tasks
+import datetime
+import base64
+from easy_pil import Editor, load_image_async, Font
+import os
+import io
+import asyncio
+import threading
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 atlanta_server = 1200772359376863272
-atlanta = None
-
+default_role = 1200844380328702113
+welcome_channel =1200773457806368820
+bot_logs = 1200845729086841012
 
 def get_token():
-  response = requests.get("https://pastebin.com/raw/THinPpik")
-  response.raise_for_status()
+  token = base64.b64decode("TVRJd01EZ3pNREV6TURBeE16QTNOelV4TlEuR1diclhDLnVYWXYxanJMTjlFY0xRaUV5dDBoXzZ2cThSS1ZsRWRBODZfaldN")
+  return token.decode("utf-8")
 
-  return response.text  # AtlantaRP bot token
+def currenttime():
+    formatted = datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp()).strftime("%H:%M:%S (%d.%m.%Y)")
+    return formatted
 
-def read_config():
-  with open("atlanta.json", "r") as config:
-    data = json.load(config)
-    return data
+async def gen_img(member):
+    try:
+        cwd = os.getcwd()
+        bg_path = os.path.join(cwd, "assets", "background.png")
+
+        bg = Editor(bg_path)
+        avatar = await load_image_async(str(member.display_avatar.url))
+        # Increase the size of the profile image slightly
+        profile_width, profile_height = (180, 180)
+
+        profile = Editor(avatar).resize((profile_width, profile_height)).circle_image()
+        bg_width, bg_height = (600, 400)
+
+        # Calculate coordinates to center the profile image
+        x_centered = (bg_width - profile_width) // 2
+        y_centered = (bg_height - profile_height) // 2
+
+        font_path = os.path.join(cwd, "assets", "font.ttf")
+
+        bg.paste(profile, (x_centered, (y_centered - 45)))
+
+        bg.text((300, 260), "Dobrodošel", color="white", font=Font(font_path, size=45), align="center", stroke_fill="black", stroke_width=5)
+        bg.text((300, 310), f"{member.display_name}", color="white", font=Font(font_path, size=45), align="center", stroke_fill="black", stroke_width=5)
+        bg.text((300, 350), "v AtlantaRP", color="white", font=Font(font_path, size=45), align="center", stroke_fill="black", stroke_width=5)
+
+        # Save the image bytes to a BytesIO buffer
+        image_buffer = io.BytesIO()
+        bg.image.save(image_buffer, format="PNG")
+
+        # Set the buffer's position to the beginning for reading
+        image_buffer.seek(0)
+
+        # Return the BytesIO buffer directly
+        return File(image_buffer, f"{member.display_name}.png")
+
+    except Exception as e:
+        print(f"Error generating image: {e}")
+        raise  # Re-raise the exception for the caller to handle
+    
+@tasks.loop(seconds=180)
+async def update_server_stats():
+  try:
+    server = discord.utils.get(bot.guilds, id=atlanta_server)
+    tracker = discord.utils.get(server.channels, id=1202260969963659304)
+    await tracker.edit(name=f"Vseh memberjev: {len(atlanta.members)}")
+    return "Successfully updated server stats"
+  except discord.HTTPException as e:
+     print(f"An error occured while updating the server stats: {e}")
+     return e
 
 @bot.event
 async def on_ready():
-  print(f"Logged in as {bot.user.name}")
-  synced = await bot.tree.sync()
-  print(f"Synced {len(synced)} commands")
-  global atlanta
-  atlanta = discord.utils.get(bot.guilds, id=atlanta_server)
+    print(f"{bot.user.display_name} ({bot.user.id}) bot logged in at {currenttime()}")
+    
+    synced = await bot.tree.sync()
+    global atlanta
+    atlanta = discord.utils.get(bot.guilds, id=atlanta_server)
+    log = discord.utils.get(atlanta.channels, id=bot_logs)
+    await log.send(content=f"**Bot logged in at {currenttime()}**\nBot info:\nUsername: {bot.user.name}\nID: {bot.user.id}\nSynced commands: {len(synced)} command(s)")
+    await update_server_stats()
 
 @bot.event
 async def on_member_join(member):
-  global atlanta
-  if member.guild.id == atlanta_server:
-    channel = discord.utils.get(atlanta.channels, id=1200773457806368820)
-    await channel.send(f"Dobrodošel {member.mention} v AtlantaRP!")
+    if member.guild.id == atlanta_server:
+        channel = discord.utils.get(atlanta.channels, id=welcome_channel)
+        role = discord.utils.get(atlanta.roles, id=default_role)
+        await member.add_roles(role)
+        member_id = member.id
+        try:
+            await channel.send(f"Dobrodošel <@{member_id}> v AtlantaRP", file=await gen_img(member))
+        except Exception as e:
+            print(f"Error generating and sending image: {e}")
 
-@bot.tree.command(name="setup1", description="Setup the bot.")
-@app_commands.describe(default_role = "Role given to members that join the server (ID)",
-                       welcome_channel = "Channel ID of the welcome channel",
-                       bot_channel = "Channel ID for the bot to send messages to")
-async def setup_server(interaction: discord.Interaction,
-                       default_role: str,
-                       welcome_channel: str,
-                       bot_channel: str):
-  if interaction.user.guild_permissions.manage_guild:
-    roles = []
-    channels = []
-    for role in atlanta.roles:
-      roles.append(role)
-      if str(role.id) != default_role:
-        roles_list = "\n".join(roles)
-        await interaction.response.send_message(f"The role you've chosen doesn't seem to exist. Here is a list of available roles and their IDs: {roles_list}")
-      else:
-        pass
-    for channel in atlanta.channels:
-      channels.append(channel)
-      if str(channel.id) != welcome_channel:
-        channels_list = "\n".join(channels)
-        await interaction.response.send_message(f"The welcome channel you've chosen doesn't seem to exist. Here is a list of available roles and their IDs: {channels_list}")
-      elif str(channel.id) != bot_channel:
-        channels_list = "\n".join(channels)
-        await interaction.response.send_message(f"The bot channel you've chosen doesn't seem to exist. Here is a list of available roles and their IDs: {channels_list}")
-      else:
-        pass
-      
-    await interaction.response.send_message(content=f"You've chosen these options:"
-                                                          f"\nDefault role: {default_role}"
-                                                          f"\nWelcome channel: {welcome_channel}"
-                                                          f"\nBot channel: {bot_channel}")
+@bot.event
+async def on_raw_member_remove(payload):
+  channel = discord.utils.get(atlanta.channels, id=welcome_channel)
+  member_id = payload.user.id
 
-    data = read_config()
-    data["default_role"] = default_role
-    data["welcome_channel"] = welcome_channel
-    data["bot_channel"] = bot_channel
+  await channel.send(content=f"<@{member_id}> nas je zapustil.")
 
-    time.sleep(3)
-    await interaction.edit_original_response(content="Updated server configuration successfully.")
-  else:
-    await interaction.response.send_message(content="You must have **Manage server** permission in order to use this command")
+"""async def cmd_exc(action=None, *args, **kwargs):
+    try:
+        if action == "rearrange":
+            if len(args) == 0:
+                # If the user just types "rearrange", return the syntax for the command
+                print("Syntax:\n'rearrange pos' - Returns a list of roles and their position integer.\n'rearrange <role-id> <pos>' - Moves role to set pos")
+            elif len(args) == 1:
+                if args == "pos":
+                    roles = atlanta.roles
+                    sorted_roles = sorted(roles, key=lambda x: x.position, reverse=True)
+                    pos_list = {role.name: role.position for role in sorted_roles}
+                    print("Current role positions:")
+                    print(pos_list)
+                    
+            elif len(args) == 2:
+                # If the user types "rearrange <role_id> <position>", move the role to the specified position
+                role_id, new_position = args
+                role_id = args[0]
+                new_position = int(new_position)
+                
+                role_to_move = discord.utils.get(atlanta.roles, id=role_id)
+                if role_to_move:
+                    await role_to_move.edit(position=new_position)
+                    print(f"Moved role {role_to_move.name} to position {new_position}")
+                    
+                    # Return the updated role positions
+                    roles = atlanta.roles
+                    sorted_roles = sorted(roles, key=lambda x: x.position, reverse=True)
+                    pos_list = {role.name: role.position for role in sorted_roles}
+                    print("Updated role positions:")
+                    print(pos_list)
+                else:
+                    print(f"Role with ID {role_id} not found.")
+            else:
+                print("Invalid number of arguments for 'rearrange' command.")
+        else:
+            print("Unknown command")
+    except Exception as e:
+        print(f"An error occured while trying to run command. {e}")"""
 
-bot.run(get_token())
+"""
+async def handle_input():
+   while True:
+      command = input(f">> ")
+      if command.lower() == "exit":
+         break
+      try:
+          parts = command.split(" ")
+          action = parts[0]
+          args = parts[1:]
+
+          await cmd_exc(action=action, *args)
+      except Exception as e:
+          print(f"An error occured when calling cmd_exc: {e}")
+"""
+
+
+if __name__ == "__main__":
+    bot.run(get_token())
